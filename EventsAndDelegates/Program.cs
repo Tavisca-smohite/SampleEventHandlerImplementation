180320356ae4ca87d86b6cbc798a351c2c75e879
tavisca-smohite <smohite@tavisca.com>
2016-09-02 01:53:51 +0530

    logic to call a method dynamically and send it to subscribe

--- EventsAndDelegates/Program.cs
+++ EventsAndDelegates/Program.cs
@@ -16,12 +16,14 @@ namespace EventsAndDelegates
             var mailService = new MailService();
             var a = typeof(MailService).GetMethod("OnVideoEncoded",new Type[] {typeof(VideoEventArgs)});
             //a.MakeGenericMethod
-           var del= Delegate.CreateDelegate(typeof(Action<MailService, VideoEventArgs>), a);
-           Action<object,object> caller = (instance,param) => del.DynamicInvoke(instance,param);
-           caller(mailService, new VideoEventArgs {Video=video });
-           // EventAggregator.Subscribe<VideoEventArgs>(caller);
-            var msgService = new MessageService();
-            var temp = new TempService();
+           var del= Delegate.CreateDelegate(typeof(Action< VideoEventArgs>),mailService,a);
+
+           Action<object> b = (param) => del.DynamicInvoke(param);
+          // Action<object,object> caller = (instance,param) => del.DynamicInvoke(instance,param);
+         //  b( new VideoEventArgs {Video=video });
+            EventAggregator.Subscribe<VideoEventArgs>(b);
+          //  var msgService = new MessageService();
+            //var temp = new TempService();
             var videoEncoder = new VideoEncoder();
             //videoEncoder.VideoEncoded += mailService.OnVideoEncoded;
             videoEncoder.Encode(video);
