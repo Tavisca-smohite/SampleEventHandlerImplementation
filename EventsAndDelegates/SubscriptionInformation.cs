2ba90ce394284e978e8300ea8ccaeb589146bfc5
tavisca-smohite <smohite@tavisca.com>
2016-09-04 23:25:50 +0530

    generate and invoke method dynamically

---
+++ EventsAndDelegates/SubscriptionInformation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsAndDelegates
+{
+    public class SubscriptionInformation
+    {
+        public MethodSignature MethodInformation { get; set; }
+
+        public Signature ClassInformation { get; set; }
+
+        public Signature EventInformation { get; set; }
+    }
+
+    public class Signature
+    {
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+    }
+
+   public class MethodSignature
+    {
+       public string Name { get; set; }
+
+       public List<string> Parameters { get; set; }
+    }
+}
